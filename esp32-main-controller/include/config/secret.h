8ed2473444cb98ad578f8607c3967b73483f60a4
triggio0 <tommy.riggio204@gmail.com>
2025-12-07 17:34:07 +0100

    hid secrets.h

--- esp32-main-controller/include/config/secret.h
+++ esp32-main-controller/include/config/secret.h
@@ -1,4 +1,2 @@
-// place secret.h in config folder
-
 const char* WiFiSsid {"my ssid"};
 const char* WiFiPassword {"my password"};
--- esp32-main-controller/include/config/secret_example.h
+++ esp32-main-controller/include/config/secret_example.h
@@ -1,2 +1,4 @@
+// place secret.h in config folder
+
 const char* WiFiSsid {"my ssid"};
 const char* WiFiPassword {"my password"};
